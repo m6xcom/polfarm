8aa26166f62867a4136c2bc30b4b06fdabd9cabc
m6xcom <65496114+m6xcom@users.noreply.github.com>
2021-05-27 22:16:40 +0300

    Updates

---
+++ js/scripts.js
@@ -0,0 +1,1 @@
+"use strict";var mobileMenuButton=document.querySelector(".mobile-menu__button");mobileMenuButton.addEventListener("click",function(){mobileMenuButton.classList.add("active")}),mobileMenuButton.addEventListener("blur",function(){mobileMenuButton.classList.remove("active")});
