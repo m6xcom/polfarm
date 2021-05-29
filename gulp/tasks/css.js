f2c644ec25b429e7dd18357cef37ba7c6f7e4cf6
m6xcom <65496114+m6xcom@users.noreply.github.com>
2021-05-29 18:43:53 +0300

    Adds code

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -1,17 +1,17 @@
-const gulp = require('gulp');
-const sourcemaps = require('gulp-sourcemaps');
-const plumber = require('gulp-plumber');
-const sass = require('gulp-sass');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-const gcmq = require('gulp-group-css-media-queries');
-const size = require('gulp-size');
+const gulp = require("gulp");
+const sourcemaps = require("gulp-sourcemaps");
+const plumber = require("gulp-plumber");
+const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const gcmq = require("gulp-group-css-media-queries");
+const size = require("gulp-size");
 // const usedcss = require('usedcss');
-const rename = require('gulp-rename');
-const mode = require('gulp-mode')();
+const rename = require("gulp-rename");
+const mode = require("gulp-mode")();
 
-const paths = require('../paths');
+const paths = require("../paths");
 
 const css = () => {
   return gulp
@@ -23,21 +23,21 @@ const css = () => {
         sourceMap: true,
         precision: 3,
         errLogToConsole: true,
-      }).on('error', sass.logError),
+      }).on("error", sass.logError)
     )
     .pipe(mode.production(gcmq()))
     .pipe(
       mode.production(
         postcss([
           // usedcss({ html: ['src/index.html'] }),
-          autoprefixer(),
+          autoprefixer({ grid: true }),
           cssnano(),
-        ]),
-      ),
+        ])
+      )
     )
     .pipe(mode.development(sourcemaps.write()))
     .pipe(size({ showFiles: true }))
-    .pipe(rename('styles.css'))
+    .pipe(rename("styles.css"))
     .pipe(gulp.dest(paths.build.css));
 };
 
